794c73cc6263bdae34082364d2bf36b3bf06f0b1
KimonePrem <Kimone.Premlall@investec.co.za>
2020-10-22 12:30:21 +0200

    feature: get data from backend for user details pop-up

--- src/app/components/user-details-modal/user-details-modal.component.ts
+++ src/app/components/user-details-modal/user-details-modal.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {UserDetailsService} from '../../services/user-details.service';
 
 @Component({
   selector: 'app-user-details-modal',
@@ -8,10 +9,29 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 })
 export class UserDetailsModalComponent implements OnInit {
 
-  constructor(public activeModal: NgbActiveModal) {
+  username: string;
+  totalSaving: number;
+  contributionDate: number;
+
+  constructor(public activeModal: NgbActiveModal,
+              private userDetailsService: UserDetailsService) {
+    this.fetchUserDetails();
   }
 
   ngOnInit(): void {
   }
 
+  private fetchUserDetails() {
+    this.userDetailsService.getUsername().then((value) => {
+      this.username = value.val();
+    });
+
+    this.userDetailsService.getTotalSavings().then((value) => {
+      this.totalSaving = Number.parseFloat(value.val());
+    });
+
+    this.userDetailsService.getContributionDate().then((value) => {
+      this.contributionDate = Number.parseFloat(value.val());
+    });
+  }
 }
